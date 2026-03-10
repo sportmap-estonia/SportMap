0ab692d8fc076d8a25567e93e67902a7fa8da1c1
arturvaldna <Artur.Valdna@Helmes.com>
2026-03-10 17:45:33 +0200

    Add credentials for postgre db

--- SportMap.AppHost/AppHost.cs
+++ SportMap.AppHost/AppHost.cs
@@ -2,24 +2,27 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var cache = builder.AddRedis("cache");
 
-var postgres = builder.AddPostgres("postgres")
-                      .WithDataVolume(isReadOnly: false);
+var pgUsername = builder.AddParameter("postgres-username", secret: true);
+var pgPassword = builder.AddParameter("postgres-password", secret: true);
 
-var postgresdb = postgres.AddDatabase("sportmapdb");
+var pgDb = builder.AddPostgres("postgres", pgUsername, pgPassword)
+    .WithDataVolume(isReadOnly: false)
+    .WithPgAdmin(pgAdmin => pgAdmin.WithHostPort(5050))
+    .AddDatabase("sportmapdb");
 
 var server = builder.AddProject<Projects.SportMap_Server>("server")
-                    .WithReference(cache)
-                    .WaitFor(cache)
-                    .WithReference(postgres)
-                    .WaitFor(postgres)
-                    .WithHttpHealthCheck("/health")
-                    .WithExternalHttpEndpoints();
+    .WithReference(cache)
+    .WaitFor(cache)
+    .WithReference(pgDb)
+    .WaitFor(pgDb)
+    .WithHttpHealthCheck("/health")
+    .WithExternalHttpEndpoints();
 
 var pnpmApp = builder.AddJavaScriptApp("webfrontend", "../frontend")
-                     .WithHttpEndpoint(port: 3000, env: "PORT")
-                     .WithReference(server)
-                     .WaitFor(server)
-                     .WithExternalHttpEndpoints()
-                     .WithPnpm();
+    .WithHttpEndpoint(port: 3000, env: "PORT")
+    .WithReference(server)
+    .WaitFor(server)
+    .WithExternalHttpEndpoints()
+    .WithPnpm();
 
 builder.Build().Run();
